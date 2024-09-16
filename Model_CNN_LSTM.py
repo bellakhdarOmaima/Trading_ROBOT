d18b93a4ca98ca3a40854cbd3b49a22599916125
bellakhdarOmaima <omaimabellakhdar@gmail.com>
2024-09-16 02:16:45 +0200

    First Commit

---
+++ Model_CNN_LSTM.py
@@ -0,0 +1,9 @@
+#Python libraries
+import matplotlib.pyplot as plt 
+import MetaTrader5 as mt5
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import tf2onnx
+from sklearn.model_selection import train_test_split
+from sys import argv
